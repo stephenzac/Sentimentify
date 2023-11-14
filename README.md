2fd3925873dd267ddd970e8c13d71c42fa671cb3
stanleyliu <stanll7@uci.edu>
2023-11-13 22:44:59 -0800

    App Bar created

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Sentimentify
+# Sentimentify 
+
+## Frontend
+After cloning from github, cd to the frontend folder and type "npm install" to install dependencies.
+
+To run the frontend, cd to the frontend folder and type "npm run dev"
---
+++ frontend/src/components/SpotifyLogoIcon.jsx
@@ -0,0 +1,13 @@
+
+import SpotifyLogo from "../assets/spotify-logo.png";
+import "../styles/SpotifyLogoIcon.css"
+
+function SpotifyLogoIcon() {
+
+    return (
+            <img id="spotify-logo-icon" src={SpotifyLogo} alt="spotify logo"/>
+    )
+
+}
+
+export default SpotifyLogoIcon;
