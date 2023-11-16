daabd8fbc1611e1c76733188a88b950dd1b2fa56
Stephen Zacarias <stephenzacarias2@gmail.com>
2023-11-16 14:42:23 -0800

    Iteration before multiprocessing

--- backend/lyrics.py
+++ backend/lyrics.py
@@ -1,6 +1,19 @@
+import requests
 from bs4 import BeautifulSoup
-import genius_api
 
 
-def get_song_lyrics():
-    pass    
+def get_song_lyrics(genius_endpoint: str) -> str:
+    """
+    Web scrape genius.com using BeautifulSoup
+    to get the lyrics of a song
+    """
+    url = "https://genius.com" + genius_endpoint
+    page = requests.get(url)
+    html = BeautifulSoup(page.content, "html.parser")
+    lyrics_element = html.find("div", {"data-lyrics-container": "true"})
+    
+    if lyrics_element:
+        lyrics = lyrics_element.get_text()
+        return lyrics
+    else:
+        return None
